707af179a1dc2ce3ebba10ecea850e001f20e283
Wemis <88230804+Wemis@users.noreply.github.com>
2021-08-26 21:19:28 +0300

    Delete luntikvzlom.py

--- luntikvzlom.py
+++
@@ -1,9 +0,0 @@
-f=input("Введите секретную команду")
-if f=="luntik vzlom start":
-	print("[*]Luntik vzlom started") 
-	while True:
-		print("String PunchTrees SSSsssPunchTrees SSSssshttps://github.com/Wemis/luntik.githttps://github.com/Wemis/luntik.git") 
-                print("VmvR9AuDp2PFg7vnb5tB55KgNA4PDs") 
-                print("import timehttps://github.com/Wemis/luntik.githttps://github.com/Wemis/luntik.git") 
-else:
-	print("Command not found.Abort") 
