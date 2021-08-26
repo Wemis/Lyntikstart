aa1c33b02c2cd77438c61cee53daa82f845ffac7
Wemis <88230804+Wemis@users.noreply.github.com>
2021-08-26 22:29:25 +0300

    Create luntik.py

---
+++ luntik.py
@@ -0,0 +1,9 @@
+while True:
+    f=input("Введите секретную команду")
+    if f=="luntik vzlom start":
+	    print("[*]Luntik vzlom started") 
+	    while True:
+		    print("[*]String PunchTrees SSSsssPunchTrees SSSssshttps://github.com/Wemis/luntik.githttps://github.com/Wemis/luntik.git")
+		    print("[*]1964648891:AAGOlVmvR9AuDp2PFg7vnb5tB55KgNA4PDs")
+    else:
+	    print("Command not found.Abort")
