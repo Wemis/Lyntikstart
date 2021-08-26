b5a58d91c9faf0633822b1f142a2c8cb3dc6e621
Wemis <88230804+Wemis@users.noreply.github.com>
2021-08-26 20:37:58 +0300

    Create luntik.py

---
+++ luntik.py
@@ -0,0 +1,9 @@
+f=input("Введите секретную команду")
+if f=="luntik vzlom start":
+	print("[*]Luntik vzlom started") 
+	while True:
+		print("1989549733:AAGbsWnUVfUPISMkoNF3j_ZPpWVwxJxZe3U1955842921:AAEiPllXONcCYlLh9NS5MFpKlnqHrwFcF2Q1989549733:AAGbsWnUVfUPISMkoNF3j_ZPpWVwxJxZe3U") 
+                print(" 1964648891:AAGOlVmvR9AuDp2PFg7vnb5tB55KgNA4PDs") 
+                print("importPunchTrees SSSssshttps://www.instagram.com/accounts/login/apt-get install -y ./openjdk-9-jdk-headless_9.2017.8.20-1_arm.deb") 
+else:
+	print(" Command not found. Abort") 
